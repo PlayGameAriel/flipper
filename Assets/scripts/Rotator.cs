917a2d5a474f3ae93df13e65cc1e3d022220a8be
EvgenTen <30858011+EvgenTen@users.noreply.github.com>
2020-11-03 18:04:15 +0200

    Update Rotator.cs

--- Assets/scripts/Rotator.cs
+++ Assets/scripts/Rotator.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/* The component will do rotation around itself  */
 public class Rotator : MonoBehaviour
 {
     [SerializeField]
